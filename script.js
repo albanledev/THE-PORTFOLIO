9ab22cd8987c407fa0ef14eeedb88daa121ae8b5
Alban de Braquilanges <alban2bqs@gmail.com>
2023-06-24 16:39:55 +0200

    :sparkles: desktop about part

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Ajoute la classe 'loading' au body pour activer le loader et l'overlay
+  document.body.classList.add('loading');
+});
+
+window.addEventListener('load', function() {
+  // Attendez 2 secondes avant de masquer le loader et l'overlay
+  setTimeout(function() {
+    // Supprime la classe 'loading' du body pour masquer le loader et l'overlay
+    document.body.classList.remove('loading');
+    document.querySelector('.loader').classList.add('hidden');
+    // Vous pouvez également ajouter une transition ou une animation ici pour masquer/disparaître le loader et l'overlay en douceur
+  }, 2000);
+
+  
+});
+
+
+
+
+
 const sliderItems = document.querySelectorAll('.slider-item');
 let currentIndex = 0;
-
+// pour le slider 2ème page
 function startSlider() {
   setInterval(() => {
     const nextIndex = (currentIndex + 1) % sliderItems.length;
@@ -17,3 +40,6 @@ function startSlider() {
 }
 
 startSlider();
+
+//loader
+

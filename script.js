43c14de5996b48da266d9aaf61184d3196b3b583
Alban de Braquilanges <alban2bqs@gmail.com>
2023-08-11 13:58:31 +0200

    :rocket: Correction

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ window.addEventListener('load', () => {
   document.body.classList.add('resize');
 });
 
+AOS.init();
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   // Ajoute la classe 'loading' au body pour activer le loader et l'overlay
   document.body.classList.add('loading');
@@ -27,7 +52,7 @@ var strings = ["Bienvenue", "Welcome"];
 var index = 0;
 var charIndex = 0;
 var delay = 100; // Délai entre chaque frappe (en ms)
-var initialDelay = 1000; // Délai initial avant de commencer l'animation (en ms)
+var initialDelay = 2000; // Délai initial avant de commencer l'animation (en ms)
 
 function typeNextChar() {
   var currentString = strings[index];
@@ -331,3 +356,19 @@ window.addEventListener('wheel', function(event) {
     });
   }
 });
+
+
+function remplacerEAccentSurPage() {
+  // Parcours de tous les éléments contenant du texte sur la page
+  var elementsTexte = document.body.getElementsByTagName('*');
+  for (var i = 0; i < elementsTexte.length; i++) {
+    var element = elementsTexte[i];
+    if (element.nodeType === Node.TEXT_NODE) {
+      // Remplacement de "É" par "E" dans le contenu de l'élément texte
+      element.nodeValue = element.nodeValue.replace(/É|é/g, 'E');
+    }
+  }
+}
+
+// Appel de la fonction au chargement de la page
+window.addEventListener('load', remplacerEAccentSurPage);
